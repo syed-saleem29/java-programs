93596f1c177a62c237cde70e3171cee53e0bf0f7
unknown <897885018qwe@gmail.com>
2023-06-13 22:20:56 +0530

    Primitive data types

---
+++ primitives.java
@@ -0,0 +1,13 @@
+public class primitives {
+    public static void main(String[] args) {
+//        primitive data types are data types which cannot be divided further
+        int rollno=119;
+        char letter ='s';
+        float marks = 6.96f;
+        double largedecimalnumber = 123464265.246541;
+        long largeinteger =160421737119L;
+        boolean check= true;
+        
+    }
+
+}
