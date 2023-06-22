7468d99674b1090fa943fe9810fd3db87efb57ad
Leyakier <897885018qwe@gmail.com>
2023-06-22 22:53:13 +0530

    CodeChef On 21/06/2023

---
+++ CodeChef_21_6_23.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+//import java.lang.;
+//import java.io.;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+          Scanner sc=new Scanner(System.in);
+//        int t=sc.nextInt();
+//        int n,x;
+//        while(t!=0){
+//            n=sc.nextInt();
+//            x=sc.nextInt();
+//            System.out.println(n-x);
+//            t--;
+//        }
+//        int t,x;
+//        t=sc.nextInt();
+//        while(t!=0){
+//            x= sc.nextInt();
+//            if(x<=3)
+//                System.out.println("BRONZE");
+//            else if(x>3&&x<=6)
+//                System.out.println("SILVER");
+//            else if(x>6)
+//                System.out.println("GOLD");
+//            t--;
+//        }
+//        String [] tabs=new String[40_000];
+//        int n=sc.nextInt();
+//        for (i=0;i<n;i++){
+//            String[i]=sc.next;
+  //      }
+
+    }
+}
